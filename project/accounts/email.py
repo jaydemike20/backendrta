5c0c170b393eca120ec20b6fbb5f84722f3a665b
jaydemike <jaydemike21@gmail.com>
2023-06-05 19:50:57 +0800

    Customize Reset Password Email

--- project/accounts/email.py
+++ project/accounts/email.py
@@ -4,6 +4,7 @@ from django.conf import settings as djangosettings
 from djoser.email import ActivationEmail
 from djoser import utils
 from djoser.conf import settings
+from djoser.email import PasswordResetEmail
 
 class CustomActivationEmail(ActivationEmail):
     template_name = "email/activation.html"
@@ -21,3 +22,23 @@ class CustomActivationEmail(ActivationEmail):
     
 class CustomConfirmationEmail(ActivationEmail):
     template_name = "email/confirmation.html"
+
+
+
+class CustomPasswordResetEmail(PasswordResetEmail):
+    template_name = "email/password_reset.html"
+
+    def get_context_data(self):
+        # PasswordResetEmail can be deleted
+        context = super().get_context_data()
+
+        user = context.get("user")
+        context['front_end_url'] = djangosettings.FRONTEND_URL
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
+        return context
+
+
+class PasswordChangedConfirmationEmail(PasswordResetEmail):
+    template_name = "email/password_changed_confirmation.html"
